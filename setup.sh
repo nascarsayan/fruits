77f44cb7e5d8370d848e1931e2e17912b5ba1ff8
Sayan Naskar <nascarsayan@gmail.com>
2022-11-12 15:53:39 +0530

    PATH

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ apt install -y curl
 curl -OJL https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 rm go1.19.3.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/environment
-source /etc/environment
+export PATH="$PATH:/usr/local/go/bin"
 go mod download
 go build -o fruits
